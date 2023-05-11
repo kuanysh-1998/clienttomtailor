3ab058260b2f3b85fc57b894577b9065e93ab026
Kuanysh <kuanysh.aptayzhanpov@mail.ru>
2023-05-11 09:51:06 +0300

    changed router

--- src/App.js
+++ src/App.js
@@ -1,47 +1,18 @@
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Products from "./pages/Products/Products";
 import Product from "./pages/Product/Product";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
-
-
-const Layout = () => {
-  return (
-    <div className="app">
-      <Navbar />
-      <Outlet />
-      <Footer />
-    </div>
-  );
-};
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/products/:id",
-        element: <Products />,
-      },
-      {
-        path: "/product/:id",
-        element: <Product />,
-      },
-    ],
-  },
-]);
+import MainLayouts from "./Layouts/MainLayouts";
 
 function App() {
   return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
+    <Routes>
+      <Route path="/" element={<MainLayouts />}>
+        <Route path="" element={<Home />} />
+        <Route path="/products/:id" element={<Products />} />
+        <Route path="/product/:id" element={<Product />} />
+      </Route>
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <PersistGate loading={"loading"} persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>
+  <HashRouter>
+    <Provider store={store}>
+      <PersistGate loading={"loading"} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </HashRouter>
 );

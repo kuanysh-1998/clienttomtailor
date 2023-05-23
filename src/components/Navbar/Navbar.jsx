4b6085e464312746c4c83ac8c2026095600e88f1
Kuanysh <kuanysh.aptayzhanpov@mail.ru>
2023-05-23 17:48:38 +0300

    started adaptive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,9 +7,11 @@ import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import Cart from "../Cart/Cart";
 import { useSelector } from "react-redux";
 import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 
 const Navbar = () => {
   const [openCart, setOpenCart] = React.useState(false);
+  const [showMenu, setShowMenu] = React.useState(false);
   const { products } = useSelector((state) => state.cartSlice);
   const { favProducts } = useSelector((state) => state.favouritesSlice);
 
@@ -17,24 +19,6 @@ const Navbar = () => {
     <div className="navbar">
       <div className="wrapper">
         <div className="navbar__left">
-          <div className="item">
-            <Link to="/products/2" className="link">
-              Женщинам
-            </Link>
-          </div>
-          <div className="item">
-            <Link to="/products/1" className="link">
-              Мужчинам
-            </Link>
-          </div>
-          <div className="item">
-            <Link to="/products/3" className="link">
-              Детям
-            </Link>
-          </div>
-        </div>
-
-        <div className="navbar__center">
           <Link to="/">
             <img
               src="https://tom-tailor.ru/local/frontend/build/images/required/logo.svg"
@@ -43,13 +27,28 @@ const Navbar = () => {
           </Link>
         </div>
 
-        <div className="navbar__right">
-          <div className="item">
+        <div className={showMenu ? "navbar__center active" : "navbar__center"}>
+          <div className="item" onClick={() => setShowMenu(!showMenu)}>
+            <Link to="/products/2" className="link">
+              Женщинам
+            </Link>
+          </div>
+          <div className="item" onClick={() => setShowMenu(!showMenu)}>
+            <Link to="/products/1" className="link">
+              Мужчинам
+            </Link>
+          </div>
+          <div className="item" onClick={() => setShowMenu(!showMenu)}>
+            <Link to="/products/3" className="link">
+              Детям
+            </Link>
+          </div>
+          <div className="item" onClick={() => setShowMenu(!showMenu)}>
             <Link className="link" to="/about">
               О компании
             </Link>
           </div>
-          <div className="item">
+          <div className="item" onClick={() => setShowMenu(!showMenu)}>
             <Link
               className="link"
               to={{
@@ -60,19 +59,28 @@ const Navbar = () => {
               Связаться с нами
             </Link>
           </div>
+        </div>
 
-          <div className="icons">
-            <SearchIcon />
-            <Link to="/favourites" className="favourites">
-              <FavoriteBorderOutlinedIcon />
-              <span>{favProducts.length}</span>
-            </Link>
-            <div className="cartIcon" onClick={() => setOpenCart(!openCart)}>
-              <ShoppingCartOutlinedIcon />
-              <span>{products.length}</span>
-            </div>
+        <div className="navbar__right">
+          <SearchIcon />
+          <Link to="/favourites" className="favourites">
+            <FavoriteBorderOutlinedIcon />
+            <span>{favProducts.length}</span>
+          </Link>
+          <div className="cartIcon" onClick={() => setOpenCart(!openCart)}>
+            <ShoppingCartOutlinedIcon />
+            <span>{products.length}</span>
+          </div>
 
-            <MenuIcon className="hamburger" />
+          <div
+            className="navbar__hamburger"
+            onClick={() => setShowMenu((prev) => !prev)}
+          >
+            {!showMenu ? (
+              <MenuIcon className="hamburger" />
+            ) : (
+              <CloseIcon className="hamburger" />
+            )}
           </div>
         </div>
       </div>
